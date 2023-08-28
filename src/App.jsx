b5b0e7163a9a4e0a4189ff3243514de45ad7bec8
Hamed Noroozi <noruzi206@gmail.com>
2023-08-28 09:49:43 +0330

    create project and add files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react'
+import './App.css'
+import Signup from './components/Signup'
+
+function App() {
+
+  return (
+    <>
+      <Signup/>
+    </>
+  )
+}
+
+export default App
