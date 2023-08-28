8b30a8c90626222a075f48737e8536efcc7d33d7
Hamed Noroozi <noruzi206@gmail.com>
2023-08-28 10:53:51 +0330

    use notify function in sign up file

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,74 +1,85 @@
 import React, { useEffect, useState } from 'react';
-import {validation} from './validation'
+
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { validation } from './validation';
+import {notify} from './toast'
+
 const Signup = () => {
-   const [data,setData]=useState({
-      name:"",
-      email:"",
-      password:"",
-      confirmPassword:"", 
-      isAccepted:false
+   const [data, setData] = useState({
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      isAccepted: false
 
    })
-   const [errors,setErrors]=useState({})
-   const [touched,setTouched]=useState({})
+   const [errors, setErrors] = useState({})
+   const [touched, setTouched] = useState({})
 
-   const focusHandler=(event)=>{
-        setTouched({...touched, [event.target.name]: true})
+   const focusHandler = (event) => {
+      setTouched({ ...touched, [event.target.name]: true })
    }
-    
-   useEffect(()=>{
+
+   useEffect(() => {
       setErrors(validation(data))
       console.log(errors);
-   },[data,touched])
+   }, [data, touched])
 
-   const changeHandler = event =>{
-     if(event.target.name === "isAccepted"){
-      setData({...data, [event.target.name]: event.target.checked})
-     }else{
-     setData({...data, [event.target.name]:event.target.value})
-     }
+   const changeHandler = event => {
+      if (event.target.name === "isAccepted") {
+         setData({ ...data, [event.target.name]: event.target.checked })
+      } else {
+         setData({ ...data, [event.target.name]: event.target.value })
+      }
    }
 
-   const submitHandler=(event)=>{
-        event.preventDefault();
-        if (!Object.keys(errors).length) {
-         
-        }else{
-         setTouched({name:true,
-         email: true,
-      password:true,
-      confirmPassword:true, 
-      isAccepted:true
-   })
-        }
+   const submitHandler = (event) => {
+      event.preventDefault();
+      notify()
+      if (!Object.keys(errors).length) {
+        notify("You sign in successfully","success")
+      } else {
+        notify("Invalid data!","error")
+         setTouched({
+            name: true,
+            email: true,
+            password: true,
+            confirmPassword: true,
+            isAccepted: true
+         })
+      }
    }
+
+
    return (
       <div>
          <form onSubmit={submitHandler}>
             <h2>Sign Up</h2>
             <div>
                <label>Name</label>
-               <input type="text" name='name' value={data.name} onChange={changeHandler} onFocus={focusHandler}/>
+               <input type="text" name='name' value={data.name} onChange={changeHandler} onFocus={focusHandler} />
                {errors.name && touched.name && <span>{errors.name}</span>}
             </div>
             <div>
                <label>Email</label>
-               <input type="text" name='email' value={data.email} onChange={changeHandler} onFocus={focusHandler}/>
+               <input type="text" name='email' value={data.email} onChange={changeHandler} onFocus={focusHandler} />
                {errors.email && touched.email && <span>{errors.email}</span>}
             </div>
             <div>
                <label>Password</label>
-               <input type="password" name='password' value={data.password} onChange={changeHandler} onFocus={focusHandler}/>
+               <input type="password" name='password' value={data.password} onChange={changeHandler} onFocus={focusHandler} />
                {errors.password && touched.password && <span>{errors.password}</span>}
             </div>
             <div>
                <label>Confirm Password</label>
-               <input type="password" name='confirmPassword' value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler}/>
+               <input type="password" name='confirmPassword' value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler} />
                {errors.confirmPassword && touched.confirmPassword && <span>{errors.confirmPassword}</span>}
             </div>
             <div>
                <label>I accept terms of privacy policy</label>
-               <input type="checkbox" name='isAccepted' value={data.isAccepted} onChange={changeHandler} onFocus={focusHandler}/>
+               <input type="checkbox" name='isAccepted' value={data.isAccepted} onChange={changeHandler} onFocus={focusHandler} />
                {errors.isAccepted && touched.isAccepted && <span>{errors.isAccepted}</span>}
             </div>
             <div>
@@ -76,6 +87,7 @@ const Signup = () => {
                <button type='submit'>Sign Up</button>
             </div>
          </form>
+         <ToastContainer />
       </div>
    );
 };
